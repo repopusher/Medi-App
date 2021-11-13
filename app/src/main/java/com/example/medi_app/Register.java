eddea13dcd88cc49c467a51849600b94964d4eea
Tobias Lennon <tobiaseastwoodlennon@gmail.com>
2021-11-13 14:20:05 +0000

    Cleaned up code

--- app/src/main/java/com/example/medi_app/Register.java
+++ app/src/main/java/com/example/medi_app/Register.java
@@ -1,88 +1,71 @@
+//Tobias Lennon
 package com.example.medi_app;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.Toast;
 import com.example.medi_app.model.User;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class Register extends AppCompatActivity implements View.OnClickListener {
 
-    private FirebaseAuth mAuth;
-    private final FirebaseDatabase database = FirebaseDatabase.getInstance("https://medi-check-76263-default-rtdb.europe-west1.firebasedatabase.app/");
     private EditText registerEmail, registerPassword, registerForename, registerSurname, registerAge;
     private RadioButton registerMale, registerFemale;
-    private ProgressBar registerProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        mAuth = FirebaseAuth.getInstance();
+        Button registerBtn  = findViewById(R.id.registerBtn);
+        registerMale        = findViewById(R.id.rdBtnMale);
+        registerFemale      = findViewById(R.id.rdBtnFemale);
+        registerForename    = findViewById(R.id.registerFirstname);
+        registerSurname     = findViewById(R.id.registerSurname);
+        registerAge         = findViewById(R.id.registerAge);
+        registerEmail       = findViewById(R.id.registerEmail);
+        registerPassword    = findViewById(R.id.registerPassword);
 
-        Button registerBtn = findViewById(R.id.registerBtn);
         registerBtn.setOnClickListener(this);
-
-        registerMale = findViewById(R.id.rdBtnMale);
-        registerFemale = findViewById(R.id.rdBtnFemale);
-        registerForename = findViewById(R.id.registerFirstname);
-        registerSurname = findViewById(R.id.registerSurname);
-        registerAge = findViewById(R.id.registerAge);
-        registerEmail = findViewById(R.id.registerEmail);
-        registerPassword = findViewById(R.id.registerPassword);
-
-        registerProgressBar = findViewById(R.id.registerProgressBar);
-
     }
 
     @Override
     public void onClick(View v){
-        switch (v.getId()){
-            case R.id.registerBtn:
+        if (v.getId() == R.id.registerBtn) {
 
-                String regAge = registerAge.getText().toString().trim();
-                String regForename = registerForename.getText().toString().trim();
-                String regSurname = registerSurname.getText().toString().trim();
-                String regEmail = registerEmail.getText().toString().trim();
-                String regPassword = registerPassword.getText().toString().trim();
-                String regSex = (registerMale.isChecked())?"M":(registerFemale.isChecked())?"F":"";
+            String regAge = registerAge.getText().toString().trim();
+            String regForename = registerForename.getText().toString().trim();
+            String regSurname = registerSurname.getText().toString().trim();
+            String regEmail = registerEmail.getText().toString().trim();
+            String regPassword = registerPassword.getText().toString().trim();
+            String regSex = (registerMale.isChecked()) ? "M" : (registerFemale.isChecked()) ? "F" : "";
 
-                Boolean validated = validateRegister(regEmail, regPassword,regForename, regSurname, regSex, regAge);
-                if(validated){
-                    //Validation is successful, call createUser() and pass return value into registerUser to authenticate and insert user into database.
-                    int age = Integer.parseInt(regAge);
-                    User user = createUser(regEmail, regForename, regSurname, regSex, age);
+            //Validating user inputs.
+            Boolean validated = validateRegister(regEmail, regPassword, regForename, regSurname, regSex, regAge);
 
-                    Intent nextReg = new Intent(Register.this, register2.class);
+            //Checks validated variable.
+            if (validated) {
 
-                    nextReg.putExtra("user", user);
-                    startActivity(nextReg);
+                int age = Integer.parseInt(regAge);
 
-//                    registerUser(regEmail, regPassword, user);
-                }
-                break;
+                //Created user to send to next register activity via intent.
+                User tempPatient = createUser(regEmail, regForename, regSurname, regSex, age);
+                Intent nextReg = new Intent(Register.this, register2.class);
 
+                //Sending data between activities.
+                nextReg.putExtra("user", tempPatient);
+                nextReg.putExtra("password", regPassword);
+                startActivity(nextReg);
+            }
         }
     }
 
-    //Method validates all the fields the patient inputs.
+    //Validates user input returns Boolean.
     private Boolean validateRegister(String regEmail, String regPassword, String regForename, String regSurname, String regSex, String regAge){
 
         //Surname validation
@@ -136,44 +119,7 @@ public class Register extends AppCompatActivity implements View.OnClickListener 
         return true;
     }
 
-    //Method authenticates the patients email and password and then inserts the patient object to
-    // the firebase realtime database in JSON format.
-    private void registerUser(String regEmail, String regPassword, User patient) {
-
-        registerProgressBar.setVisibility(View.VISIBLE);
-
-        mAuth.createUserWithEmailAndPassword(regEmail, regPassword)
-                .addOnCompleteListener(task -> {
-
-                    //If authentication is successful, create a user and store it in the realtime database.
-                    if(task.isSuccessful()){
-                        database.getReference("Patients").child(FirebaseAuth.getInstance().getCurrentUser()
-                                .getUid()).setValue(patient).addOnCompleteListener(registerTask -> {
-                                    if(registerTask.isSuccessful()){
-
-                                        registerProgressBar.setVisibility(View.GONE);
-                                        Toast.makeText(Register.this, "Successfully registered", Toast.LENGTH_LONG).show();
-
-                                        Intent switchActivity = new Intent(Register.this, Index.class);
-                                        startActivity(switchActivity);
-                                    }
-                                    else
-                                    {
-                                        Toast.makeText(Register.this, "Something went wrong :(", Toast.LENGTH_LONG).show();
-                                        registerProgressBar.setVisibility(View.GONE);
-                                    }
-                        });
-                    }
-                    else{
-                        Toast.makeText(Register.this, "Failed to register.", Toast.LENGTH_LONG).show();
-                        registerProgressBar.setVisibility(View.GONE);
-                    }
-                });
-
-
-    }
-
-    //Method instantiates and returns User object with empty healthInsurance field.
+    //Instantiates a User and returns it.
     public User createUser(String registerEmail, String registerForename, String registerSurname, String registerSex, int registerAge ){
         return new User(registerEmail, registerForename, registerSurname, registerSex, registerAge, "", "");
     }
--- app/src/main/java/com/example/medi_app/register2.java
+++ app/src/main/java/com/example/medi_app/register2.java
@@ -1,52 +1,64 @@
+//Tobias Lennon
 package com.example.medi_app;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
-
 import com.example.medi_app.model.GP;
 import com.example.medi_app.model.InsuranceCompany;
 import com.example.medi_app.model.User;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import java.util.ArrayList;
 
-public class register2 extends AppCompatActivity implements View.OnClickListener{
-    private final ArrayList<String> spinnerDisplayInsruance = new ArrayList<>();
-    private final ArrayList<String> spinnerDisplayGP = new ArrayList<>();
-    private final ArrayList<String> GPkeys = new ArrayList<>();
-    private final ArrayList<String> insuranceKeys = new ArrayList<>();
-    private final FirebaseDatabase database = FirebaseDatabase.getInstance("https://medi-check-76263-default-rtdb.europe-west1.firebasedatabase.app/");
-    private Spinner spinnerInsurance, spinnerGPs;
-    private Button regBtn;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class register2 extends AppCompatActivity implements View.OnClickListener {
+
+    private final ArrayList<String> spinnerDisplayInsurance = new ArrayList<>();
+    private final ArrayList<String> spinnerDisplayGP        = new ArrayList<>();
+    private final ArrayList<String> GPkeys                  = new ArrayList<>();
+    private final ArrayList<String> insuranceKeys           = new ArrayList<>();
+    private final FirebaseDatabase  database                = FirebaseDatabase.getInstance("https://medi-check-76263-default-rtdb.europe-west1.firebasedatabase.app/");
+    private ProgressBar registerProgressBar;
     private int insurancePos, GPpos;
+    private FirebaseAuth mAuth;
+    private User patient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
 
-        spinnerInsurance = findViewById(R.id.spinnerReg);
-        spinnerGPs = findViewById(R.id.spinnerReg2);
-        regBtn = findViewById(R.id.registerBtn2);
-        regBtn.setOnClickListener(this);
+        Spinner spinnerInsurance = findViewById(R.id.spinnerReg);
+        Spinner spinnerGPs       = findViewById(R.id.spinnerReg2);
+        Button regBtn            = findViewById(R.id.registerBtn2);
+        registerProgressBar      = findViewById(R.id.progressBar);
+        mAuth                    = FirebaseAuth.getInstance();
+        patient                  = getIntent().getParcelableExtra("user");
 
-        User patient = getIntent().getParcelableExtra("user");
+        regBtn.setOnClickListener(this);
+        registerProgressBar.setVisibility(View.GONE);
 
         readDatabase("Insurance", spinnerInsurance);
         readDatabase("GPs", spinnerGPs);
 
-
+        //Get position of selected spinner item and uses that position as an index in a list of
+        //GP and insurance IDs to then assign to the patient.
         spinnerInsurance.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -73,25 +85,26 @@ public class register2 extends AppCompatActivity implements View.OnClickListener
     }
 
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         if (v.getId() == R.id.registerBtn2) {
 
-            String pos = String.valueOf(insurancePos);
-            String pos1 = String.valueOf(GPpos);
-            String pos2 = pos + " " + pos1;
-            Toast.makeText(this, pos2, Toast.LENGTH_SHORT).show();
-            }
-        }
+            patient.setHealthInsurance(insuranceKeys.get(insurancePos));
+            patient.setGPId(GPkeys.get(GPpos));
+            String regPassword = getIntent().getStringExtra("password");
+            registerUser(regPassword, patient);
 
-    private void readDatabase(String branch, Spinner spinner){
+        }
+    }
+
+    //Reads database based on the branch passed, puts read items into adapter and sets onto view.
+    private void readDatabase(String branch, Spinner spinner) {
         DatabaseReference myRef = database.getReference().child(branch);
         ArrayAdapter<String> adapter;
 
-        if(branch.equals("Insurance")) {
-            adapter = new ArrayAdapter<String>(register2.this, android.R.layout.simple_list_item_1, spinnerDisplayInsruance);
-        }
-        else {
-            adapter = new ArrayAdapter<String>(register2.this, android.R.layout.simple_list_item_1, spinnerDisplayGP);
+        if (branch.equals("Insurance")) {
+            adapter = new ArrayAdapter<>(register2.this, android.R.layout.simple_list_item_1, spinnerDisplayInsurance);
+        } else {
+            adapter = new ArrayAdapter<>(register2.this, android.R.layout.simple_list_item_1, spinnerDisplayGP);
         }
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -101,24 +114,24 @@ public class register2 extends AppCompatActivity implements View.OnClickListener
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (branch.equals("Insurance")) {
-                    spinnerDisplayInsruance.clear();
+                    spinnerDisplayInsurance.clear();
                     insuranceKeys.clear();
-                }
-                else {
+                } else {
                     spinnerDisplayGP.clear();
                     GPkeys.clear();
                 }
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
 
-                for (DataSnapshot snapshot: dataSnapshot.getChildren()){
-
-                    switch (branch){
+                    switch (branch) {
                         case "Insurance":
                             InsuranceCompany insurance = snapshot.getValue(InsuranceCompany.class);
-                            spinnerDisplayInsruance.add(insurance.getHcName());
+                            assert insurance != null;
+                            spinnerDisplayInsurance.add(insurance.getHcName());
                             insuranceKeys.add(snapshot.getKey());
                             break;
                         case "GPs":
                             GP gp = snapshot.getValue(GP.class);
+                            assert gp != null;
                             spinnerDisplayGP.add(gp.getName() + ", " + gp.getPhone());
                             GPkeys.add(snapshot.getKey());
                             break;
@@ -131,6 +144,36 @@ public class register2 extends AppCompatActivity implements View.OnClickListener
             public void onCancelled(@NonNull DatabaseError error) {
                 // Failed to read value
                 Log.w("ERROR\t", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    //Method authenticates the patients email and password and then inserts the patient object to
+    // the firebase realtime database in JSON format.
+    private void registerUser(String regPassword, User patient) {
+
+        registerProgressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(patient.getEmail(), regPassword).addOnCompleteListener(task -> {
+
+            //If authentication is successful, create a user and store it in the realtime database.
+            if (task.isSuccessful()) {
+                database.getReference("Patients").child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser())
+                        .getUid()).setValue(patient).addOnCompleteListener(registerTask -> {
+                            if (registerTask.isSuccessful()) {
+                                registerProgressBar.setVisibility(View.GONE);
+                                Toast.makeText(register2.this, "Successfully registered", Toast.LENGTH_LONG).show();
+
+                                Intent switchActivity = new Intent(register2.this, Index.class);
+                                startActivity(switchActivity);
+                            } else {
+                                Toast.makeText(register2.this, "Something went wrong :(", Toast.LENGTH_LONG).show();
+                                registerProgressBar.setVisibility(View.GONE);
+                            }
+                        });
+            } else {
+                Toast.makeText(register2.this, "Failed to register.", Toast.LENGTH_LONG).show();
+                registerProgressBar.setVisibility(View.GONE);
             }
         });
     }
